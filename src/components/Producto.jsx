bb7a42c4f30318fd0db4b4e15e1306546cf5efae
aUreLi0triste <samanthacarome@gmail.com>
2025-10-15 15:57:05 -0500

    Parte hecha por Samantha Rodriguez

---
+++ src/components/Producto.jsx
@@ -0,0 +1,39 @@
+function Producto({
+  id,
+  nombre,
+  categoria,
+  precio,
+  cantidad,
+  seleccionado,
+  imagen,
+  onCantidadChange,
+  onSeleccionChange
+}) {
+  return (
+    <div className="producto">
+      <input
+        type="checkbox"
+        className="seleccion"
+        checked={seleccionado}
+        onChange={() => onSeleccionChange(id)}
+      />
+      <img src={imagen} alt={nombre} />
+      <div className="info">
+        <p><b>{nombre}</b></p>
+        <p>{categoria}</p>
+        <p className="precio" data-precio={precio}>S/ {precio}</p>
+        <div className="cantidad">
+          <label>Cantidad:</label>
+          <input
+            type="number"
+            value={cantidad}
+            min="1"
+            onChange={(e) => onCantidadChange(id, e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Producto;
